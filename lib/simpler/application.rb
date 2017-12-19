c3d0a7863d0ef1e6a21bae73fe055ff130a045fa
Evgeniy Fateev <spaceflow@gmail.com>
2017-12-19 03:54:30 +0300

    Add Sequel setup and model

--- lib/simpler/application.rb
+++ lib/simpler/application.rb
@@ -1,4 +1,6 @@
+require 'yaml'
 require 'singleton'
+require 'sequel'
 require_relative 'router'
 require_relative 'controller'
 
@@ -7,11 +9,15 @@ module Simpler
 
     include Singleton
 
+    attr_reader :db
+
     def initialize
       @router = Router.new
+      @db = nil
     end
 
     def bootstrap!
+      setup_database
       require_app
       require_routes
     end
@@ -36,6 +42,12 @@ module Simpler
 
     def require_routes
       require Simpler.root.join('config/routes')
+    end
+
+    def setup_database
+      database_config = YAML.load_file(Simpler.root.join('config/database.yml'))
+      database_config['database'] = Simpler.root.join(database_config['database'])
+      @db = Sequel.connect(database_config)
     end
 
     def make_response(controller, action)
