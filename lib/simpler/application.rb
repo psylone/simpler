230232048d6616db50a1b5841a99bb0656502d94
Maksimov Konstantin <kmaksimovv@gmail.com>
2019-01-11 15:05:24 +0400

    task if url not found

--- lib/simpler/application.rb
+++ lib/simpler/application.rb
@@ -27,11 +27,14 @@ module Simpler
     end
 
     def call(env)
-      route = @router.route_for(env)
-      controller = route.controller.new(env)
-      action = route.action
-
-      make_response(controller, action)
+      begin
+        route = @router.route_for(env)
+        controller = route.controller.new(env)
+        action = route.action
+        make_response(controller, action)
+      rescue NoMethodError
+        not_found_route
+      end 
     end
 
     private
@@ -54,5 +57,8 @@ module Simpler
       controller.make_response(action)
     end
 
+    def not_found_route
+      [ 404, { "Content-Type" => "text/plain" }, ["Not found"] ]
+    end
   end
 end
