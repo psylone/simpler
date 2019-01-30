47e1c72f138f0dfd728b5182bb383e3bc6705a6b
Egor Nagornov <nagornov_e@bk.ru>
2019-01-30 10:03:07 +0700

    Route exception

--- lib/simpler/application.rb
+++ lib/simpler/application.rb
@@ -28,13 +28,25 @@ module Simpler
 
     def call(env)
       route = @router.route_for(env)
-      controller = route.controller.new(env)
-      action = route.action
 
-      make_response(controller, action)
+      if route
+        controler = route.controller.new(env)
+        action = route.action
+        make_response(controller, action)
+      else
+        url_not_found_response
+      end
     end
 
     private
+
+    def url_not_found_response
+      [
+        404,
+        { 'Content-Type' => 'text/plain' },
+        ["Error: 404\nURL not found\n"]
+      ]
+    end
 
     def require_app
       Dir["#{Simpler.root}/app/**/*.rb"].each { |file| require file }
