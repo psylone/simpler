631899e62a5ec74dee29dfcb6eb1c29d3626d5b6
ziminator <aleksander.zimin@gmail.com>
2019-08-16 22:59:40 +0300

    Homework

--- lib/simpler/application.rb
+++ lib/simpler/application.rb
@@ -28,13 +28,24 @@ module Simpler
 
     def call(env)
       route = @router.route_for(env)
-      controller = route.controller.new(env)
-      action = route.action
-
-      make_response(controller, action)
+      if route
+        controller = route.controller.new(env)
+        action = route.action
+        make_response(controller, action)
+      else
+        not_found
+      end
     end
 
     private
+
+    def not_found
+      [
+        404,
+        { 'Content-Type' => 'text/plain' }
+        ['Not found']
+      ]
+    end
 
     def require_app
       Dir["#{Simpler.root}/app/**/*.rb"].each { |file| require file }
