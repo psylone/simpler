bdd2c743c5575f7e85dd2e78cbad8d13db85df64
Vadim Savostin <vsavdev@gmail.com>
2019-10-19 03:35:14 +0300

    add test actions

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -2,10 +2,16 @@ class TestsController < Simpler::Controller
 
   def index
     @time = Time.now
+    render plain: 'plain text message'
   end
 
-  def create
+  def create; end
 
+  def show
+    # render plain: "Test: #{params[:id]}"
+    @params = params[:id]
+    status(201)
+    set_header('Test-Header', 'text/html')
   end
 
 end
