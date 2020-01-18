3277e87bf6ac4a37f87602dd9fe3d799353f4f1f
Roman <zworkbox@yandex.ru>
2020-01-18 03:26:04 +0300

    p20 some refactor

--- lib/extentions/events.rb
+++ lib/extentions/events.rb
@@ -5,22 +5,26 @@ module Events
   end
 
   module ClassMethods
-    EVENT_NAME_MUST_BE_SYMBOL = "Имя события должно быть символом"
+    EVENT_NAME_MUST_BE_SYMBOL = 'Имя события должно быть символом'.freeze
 
     def event(event_name)
-      raise TypeError, EVENT_NAME_MUST_BE_SYMBOL unless event_name.is_a?(Symbol)
+      must_be_symbol!(event_name)
 
       event_var = "@#{event_name}".to_sym
 
       define_method(event_name) do
         instance_variable_get(event_var) || instance_variable_set(event_var, [])
       end
-    end  
+    end
+
+    def must_be_symbol!(event_name)
+      raise TypeError, EVENT_NAME_MUST_BE_SYMBOL unless event_name.is_a?(Symbol)
+    end
   end
-  
+
   def when_fired(event_name, &command)
-    raise TypeError, EVENT_NAME_MUST_BE_SYMBOL unless event_name.is_a?(Symbol)
-    
+    self.class.must_be_symbol!(event_name)
+
     event_subscribers = send(event_name)
     event_subscribers << command unless event_subscribers.include?(command)
   end
@@ -28,14 +32,14 @@ module Events
   def on(event_name, &command)
     when_fired(event_name, &command)
   end
-  
+
   def fire(event_name, *args)
-    raise TypeError, EVENT_NAME_MUST_BE_SYMBOL unless event_name.is_a?(Symbol)
+    self.class.must_be_symbol!(event_name)
 
     event_subscribers = send(event_name)
     event_subscribers.each do |subscriber|
       subscriber.call(*args)
     end
   end
-  
+
 end
