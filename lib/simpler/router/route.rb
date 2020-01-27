1aa70b9b44396b3294faec48e8f8e4ef9062f456
vesh95 <vesh95.17@ya.ru>
2020-01-27 20:32:29 +0500

    changed match? Route method

--- lib/simpler/router/route.rb
+++ lib/simpler/router/route.rb
@@ -12,9 +12,35 @@ module Simpler
       end
 
       def match?(method, path)
-        @method == method && path.match(@path)
+        return if @method != method
+
+        parsed_url = parse(path)
+        parsed_path = parse(@path)
+
+        return if parsed_url.size != parsed_path.size
+
+        return if equal_path(parsed_path, parsed_url).include?(false)
+
+        true
       end
 
+      def params(path_from_url)
+        parsed_path = parse @path
+        parsed_url = parse path_from_url
+        parsed_path.map.with_index.with_object({}) do |(p, i), param|
+          param[p[1..-1]] = parsed_url[i] if p =~ /\:.+/
+        end
+      end
+
+      private
+
+      def equal_path(path, route)
+        route.map.with_index { |u, i| (u == path[i]) || (path[i] =~ /\:.+/) }
+      end
+
+      def parse(path)
+        path.split('/').reject(&:empty?)
+      end
     end
   end
 end
