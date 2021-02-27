f052355841472c44eb594544ef5e0359647548a2
banpresto <sexyteemo@gmail.com>
2021-02-27 18:47:55 +0300

    lesson_20

---
+++ lib/middleware/logger.rb
@@ -0,0 +1,25 @@
+require 'logger'
+
+class AppLogger
+
+  def initialize(app, **options)
+    @logger = Logger.new(options[:logdev] || STDOUT)
+    @app = app    
+  end
+
+  def call(env)
+    status, headers, response = @app.call(env)
+    @logger.info(message(status, headers, env))
+
+    [status, headers, response]
+  end
+
+  private
+
+  def message(status, headers, env)
+    "\nRequest: #{env['REQUEST_METHOD']} #{env['REQUEST_URI']}\n" \
+    "Handler: #{env['simpler.controller'].class}##{env['simpler.action']}\n" \
+    "Parameters: #{env['simpler.params']}\n" \
+    "Response: #{status} OK [#{headers['Content-Type']}] #{env['simpler.render']}\n" \
+  end
+end
--- lib/simpler/controller.rb
+++ lib/simpler/controller.rb
@@ -14,6 +14,7 @@ module Simpler
     def make_response(action)
       @request.env['simpler.controller'] = self
       @request.env['simpler.action'] = action
+      @request.env['simpler.params'].each { |key, value| @request.update_param(key.to_sym, value) }
 
       set_default_headers
       send(action)
@@ -28,8 +29,16 @@ module Simpler
       self.class.name.match('(?<name>.+)Controller')[:name].downcase
     end
 
+    def set_status(code)
+      @response.status = code
+    end
+
     def set_default_headers
-      @response['Content-Type'] = 'text/html'
+      set_header('Content-Type', 'text/html')
+    end
+
+    def set_header(key, value)
+      @response[key] = value
     end
 
     def write_response
@@ -39,14 +48,31 @@ module Simpler
     end
 
     def render_body
-      View.new(@request.env).render(binding)
+      View.new(@request.env, @render_type).render(binding)
     end
 
     def params
-      @request.params
+      @request.env['simpler.route.params'].merge!@request.params
     end
 
     def render(template)
+      @request.env['simpler.template'] = template
+      
+      if template.is_a?(String)
+        @render_type = 'html'
+        render_html(template)
+      elsif template.keys.first
+        @render_type = 'plain'
+        render_plain(template)
+      end
+    end
+
+    def render_plain(template)
+      set_header('Content-Type', 'text/html')
+      @request.env['simpler.template'] = template[:plain]
+    end
+
+    def render_html(template)
       @request.env['simpler.template'] = template
     end
 
