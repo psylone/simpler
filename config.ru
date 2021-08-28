a74e43324c5cdc30c3f57d046be325ed309a3589
Tagir <tagirbpl@gmail.com>
2021-08-28 15:07:54 +0300

    Add http logger

--- config.ru
+++ config.ru
@@ -1,3 +1,5 @@
 require_relative 'config/environment'
+require_relative 'lib/middleware/http_logger'
 
+use HttpLogger
 run Simpler.application
---
+++ lib/middleware/http_logger.rb
@@ -0,0 +1,25 @@
+require 'logger'
+
+class HttpLogger
+  def initialize(app)
+    @app = app
+    @logger = Logger.new(Simpler.root.join('log/app.log'))
+  end
+
+  def call(env)
+    status, headers, body = @app.call(env)
+
+    @logger.info(log(env, status, headers))
+    
+    [status, headers, body]
+  end
+
+  def log(env, status, headers)
+    {
+      Request: "#{env['REQUEST_METHOD']} #{env['REQUEST_PATH']}",
+      Handler: "#{env['simpler.controller'].class}##{env['simpler.action']}",
+      Parameters: env['simpler.params'],
+      Response: "#{status} [#{headers['Content-Type']}] #{env['simpler.template']}"
+    }
+  end
+end
