592e478f52aadd42f6a89dfe876eb5a3bb1b9675
Rustam <bashbez.ru@yandex.ru>
2022-04-11 17:30:16 +0500

    1

--- lib/simpler/view.rb
+++ lib/simpler/view.rb
@@ -2,7 +2,6 @@ require 'erb'
 
 module Simpler
   class View
-
     VIEW_BASE_PATH = 'app/views'.freeze
 
     def initialize(env)
@@ -10,12 +9,24 @@ module Simpler
     end
 
     def render(binding)
+      if template.is_a?(Hash)
+        template.values[0]
+      else
+        render_partial(binding)
+      end
+    end
+
+    private
+
+    def render_partial(binding)
       template = File.read(template_path)
 
       ERB.new(template).result(binding)
     end
 
-    private
+    def template
+      @env['simpler.template']
+    end
 
     def controller
       @env['simpler.controller']
@@ -25,15 +36,11 @@ module Simpler
       @env['simpler.action']
     end
 
-    def template
-      @env['simpler.template']
-    end
-
     def template_path
       path = template || [controller.name, action].join('/')
+      @env['simpler.template_path'] = "#{path}.html.erb"
 
-      Simpler.root.join(VIEW_BASE_PATH, "#{path}.html.erb")
+      Simpler.root.join(VIEW_BASE_PATH, @env['simpler.template_path'])
     end
-
   end
 end
