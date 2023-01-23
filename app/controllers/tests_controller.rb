3ae1db67e3eaf2796d5f4fd9f3287c5a21cde408
VadimZhukov <vadim.zhukov@gmail.com>
2023-01-23 23:34:16 +0300

    feat: status method

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -3,6 +3,7 @@ class TestsController < Simpler::Controller
   def index
     @time = Time.now
     render 'tests/list'
+    status 201
   end
 
   def create
--- lib/simpler/controller.rb
+++ lib/simpler/controller.rb
@@ -73,5 +73,9 @@ module Simpler
       end
     end
 
+    def status(status_code)
+      @response.status = status_code
+    end
+
   end
 end
