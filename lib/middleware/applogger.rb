8933ff611a36126be4e590db7fb6929f16a569c9
Tavbulatov <alikhantam26@gmail.com>
2023-03-25 02:08:45 +0300

    added a parameter to the applogger constructor to work with the logger object

--- lib/middleware/applogger.rb
+++ lib/middleware/applogger.rb
@@ -1,9 +1,12 @@
 class AppLogger
-  def initialize(app)
+  def initialize(app, **options)
+    @logger = Logger.new(options[:logdev] || STDOUT)
     @app = app
   end
 
   def call(env)
     status, headers, body = @app.call(env)
+
+    [status, headers, [body]]
   end
 end
