2346f15631c72dfafbd137a5268eabab4a2e7d7c
Tavbulatov <alikhantam26@gmail.com>
2023-03-26 00:59:38 +0300

    creation of middleware for logging HTTP requests and responses

--- lib/middleware/applogger.rb
+++ lib/middleware/applogger.rb
@@ -7,6 +7,25 @@ class AppLogger
   def call(env)
     status, headers, body = @app.call(env)
 
-    [status, headers, [body]]
+    request = Rack::Request.new(env)
+
+    request_info(request)
+    responce_info(status, headers, request)
+
+    [status, headers, ["#{body}"]]
+  end
+
+  def request_info(request)
+    log = "Request: #{request.env["REQUEST_METHOD"]} #{request.env["REQUEST_URI"]}
+                                                  Handler: #{request.env['simpler.controller'].class.name}##{request.env['simpler.action']}
+                                                  Parameters: #{request.params}"
+
+    @logger.info(log)
+  end
+
+  def responce_info(status, headers, request)
+    log = "Response: #{status} #{headers["Content-Type"]} #{request.env['simpler.file_path']} "
+
+    @logger.info(log) if request.env['simpler.file_path']
   end
 end
