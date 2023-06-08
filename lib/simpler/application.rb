cf2506742cf71386664233180007b84b9027a3b0
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2023-06-08 09:37:39 +0300

    add not found response

--- lib/simpler/application.rb
+++ lib/simpler/application.rb
@@ -28,10 +28,14 @@ module Simpler
 
     def call(env)
       route = @router.route_for(env)
-      controller = route.controller.new(env)
-      action = route.action
 
-      make_response(controller, action)
+      if route
+        controller = route.controller.new(env)
+        action = route.action
+        make_response(controller, action)
+      else
+        not_found_response
+      end
     end
 
     private
@@ -52,6 +56,10 @@ module Simpler
 
     def make_response(controller, action)
       controller.make_response(action)
+    end
+
+    def not_found_response
+      [404, {'Content-Type' => 'text/plain'}, ['Not found (404 Error)']]
     end
 
   end
