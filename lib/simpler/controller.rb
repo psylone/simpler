0aeccdc409bbbcd607bd574bd1d0fd8fc6696149
Runidan <45917212+Runidan@users.noreply.github.com>
2024-02-04 12:52:05 +0500

    add render some formats methods

--- lib/simpler/controller.rb
+++ lib/simpler/controller.rb
@@ -3,6 +3,12 @@ require_relative 'view'
 module Simpler
   class Controller
 
+    RENDER_FORMATS = {
+      :plain => :render_plain, 
+      :json => :render_json,
+      :html => :render_html
+    }
+
     attr_reader :name, :request, :response
 
     def initialize(env)
@@ -46,9 +52,26 @@ module Simpler
       @request.params
     end
 
-    def render(template)
-      @request.env['simpler.template'] = template
+    def render(parametr, **args)
+      if parametr.is_a?(String)
+        @request.env['simpler.template'] = parametr
+      elsif parametr.is_a?(Hash)
+        format_method = RENDER_FORMATS.fetch(parametr.keys.first, :render_error)
+        format_value = parametr[parametr.keys.first]
+        self.method(format_method).call(format_value, **args)
+      end
     end
+
+    def render_plain(text, **args)
+      @request.env['simpler.template'] = text
+    end
+
+    def render_json(text, **args)
+
+    end
+
+
+
 
   end
 end
