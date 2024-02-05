39a7556b8b0b69baa6979c61d559215cd5d72318
vitaliy.supron <vitaliy.supron@karuna.group>
2024-02-05 17:49:53 +0300

    add render plain
    add render plain
    
    add render plain

--- lib/simpler/view.rb
+++ lib/simpler/view.rb
@@ -1,4 +1,5 @@
-require 'erb'
+require_relative 'view/html_render'
+require_relative 'view/plain_render'
 
 module Simpler
   class View
@@ -10,9 +11,7 @@ module Simpler
     end
 
     def render(binding)
-      template = File.read(template_path)
-
-      ERB.new(template).result(binding)
+      render_template.result(binding)
     end
 
     private
@@ -29,11 +28,23 @@ module Simpler
       @env['simpler.template']
     end
 
+    def render_template
+      template = @env['simpler.template']
+
+      case template
+      when String
+        HtmlRender.new(template_path)
+      when Hash
+        PlainRender.new(template) if template.has_key?(:plain)
+      else
+        HtmlRender.new(template_path)
+      end
+    end
+
     def template_path
       path = template || [controller.name, action].join('/')
 
       Simpler.root.join(VIEW_BASE_PATH, "#{path}.html.erb")
     end
-
   end
 end
