382ae920bc5d15e7b81a7fab1dc78c558c545a2d
Runidan <45917212+Runidan@users.noreply.github.com>
2024-02-11 12:43:38 +0500

    add render :plain

--- lib/simpler/controller.rb
+++ lib/simpler/controller.rb
@@ -3,12 +3,6 @@ require_relative 'view'
 module Simpler
   class Controller
 
-    RENDER_FORMATS = {
-      :plain => :render_plain, 
-      :json => :render_json,
-      :html => :render_html
-    }
-
     attr_reader :name, :request, :response
 
     def initialize(env)
@@ -21,7 +15,6 @@ module Simpler
       @request.env['simpler.controller'] = self
       @request.env['simpler.action'] = action
 
-      set_default_headers
       send(action)
       write_response
 
@@ -42,10 +35,6 @@ module Simpler
       self.class.name.match('(?<name>.+)Controller')[:name].downcase
     end
 
-    def set_default_headers
-      @response['Content-Type'] = 'text/html'
-    end
-
     def write_response
       body = render_body
 
@@ -55,6 +44,7 @@ module Simpler
     def render_body
       view = View.new(@request.env)
       @response['Rander-Template-Path'] = view.used_template.to_s
+      @response['Content-Type'] = view.content_type.to_s
       view.render(binding)
     end
 
@@ -64,22 +54,6 @@ module Simpler
 
     def render(parametr)
       @request.env['simpler.template'] = parametr
-      # if parametr.is_a?(String)
-      #   @request.env['simpler.template'] = parametr
-      # elsif parametr.is_a?(Hash)
-      #   format_method = RENDER_FORMATS.fetch(parametr.keys.first, :render_error)
-      #   format_value = parametr[parametr.keys.first]
-      #   self.method(format_method).call(format_value, **args)
-      # end
     end
-
-    def render_plain(text, **args)
-      @request.env['simpler.template'] = text
-    end
-
-    def render_json(text, **args)
-
-    end
-
   end
 end
--- lib/simpler/view.rb
+++ lib/simpler/view.rb
@@ -5,20 +5,27 @@ module Simpler
 
     VIEW_BASE_PATH = 'app/views'.freeze
 
-    attr_reader :used_template
+    attr_reader :used_template, :content_type
 
     def initialize(env)
       @env = env
       @used_template = template_path
+      @content_type = 'text/html'
     end
 
     def render(binding)
-      template = File.read(@used_template)
+      return plain_render if template.is_a?(Hash) && template.has_key?(:plain)
+      template_file = File.read(@used_template)
 
-      ERB.new(template).result(binding)
+      ERB.new(template_file ).result(binding)
     end
 
     private
+
+    def plain_render
+      @content_type = 'text/plain'
+      template[:plain]
+    end
 
     def controller
       @env['simpler.controller']
